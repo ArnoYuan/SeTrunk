4ff77b236e45759274e3a9eebb5b579eee36c461
cyberniklee <cyberniklee@gmail.com>
2017-10-11 04:57:48 -0700

    first commit

---
+++ Source/TrunkApplication.cpp
@@ -0,0 +1,76 @@
+/*
+ * TrunkApplication.cpp
+ *
+ *  Created on: Oct 11, 2017
+ *      Author: root
+ */
+
+#include "TrunkApplication.h"
+#include <Parameter/Parameter.h>
+#include <MapGenerator/MapGenerator.h>
+
+namespace NS_Trunk
+{
+
+  TrunkApplication::TrunkApplication()
+  {
+    map_cli = new NS_Service::Client<NS_ServiceType::ServiceMap>("MAP");
+  }
+
+  TrunkApplication::~TrunkApplication()
+  {
+    if(running)
+    {
+      running = false;
+      map_generate_thread.join ();
+    }
+    delete map_cli;
+  }
+
+  void TrunkApplication::loadParameters()
+  {
+    NS_NaviCommon::Parameter parameter;
+    parameter.loadConfigurationFile("trunk.xml");
+
+    map_path_ = parameter.getParameter("map_path", "/tmp/gmap.pgm");
+    map_update_rate_ = parameter.getParameter("map_update_rate", 1.0f);
+  }
+
+  void TrunkApplication::mapGenerateLoop()
+  {
+    NS_NaviCommon::Rate rate(map_update_rate_);
+    while(running)
+    {
+      NS_ServiceType::ServiceMap map;
+      if(map_cli->call(map))
+      {
+        if(map.result)
+        {
+          NS_NaviCommon::MapGenerator::saveMapInPGM(map.map.data, map.map.info.height, map.map.info.width, map_path_);
+        }
+      }
+      rate.sleep();
+    }
+  }
+
+  void TrunkApplication::run()
+  {
+    console.message ("Trunk application is running!");
+
+    loadParameters();
+
+    running = true;
+
+    map_generate_thread = boost::thread (boost::bind (&TrunkApplication::mapGenerateLoop, this));
+  }
+
+  void TrunkApplication::quit()
+  {
+    console.message ("Trunk application is quitting!");
+
+    running = false;
+
+    map_generate_thread.join ();
+  }
+
+} /* namespace NS_Trunk */
---
+++ Source/TrunkApplication.h
@@ -0,0 +1,51 @@
+/*
+ * TrunkApplication.h
+ *
+ *  Created on: Oct 11, 2017
+ *      Author: root
+ */
+
+#ifndef _TRUNK_APPLICATION_H_
+#define _TRUNK_APPLICATION_H_
+
+#include <Application/Application.h>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+
+#include <Service/ServiceType/ServiceMap.h>
+#include <Service/Client.h>
+
+#include <iostream>
+#include <string>
+
+namespace NS_Trunk
+{
+
+  class TrunkApplication: public Application
+  {
+  public:
+    TrunkApplication();
+    virtual ~TrunkApplication();
+
+  private:
+    std::string map_path_;
+    double map_update_rate_;
+
+  private:
+    NS_Service::Client<NS_ServiceType::ServiceMap>* map_cli;
+
+    boost::thread map_generate_thread;
+
+  private:
+    void loadParameters();
+
+    void mapGenerateLoop();
+
+  public:
+    virtual void run();
+    virtual void quit();
+  };
+
+} /* namespace NS_Trunk */
+
+#endif /* TRUNKAPPLICATION_H_ */
